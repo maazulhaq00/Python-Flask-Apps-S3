2d497db812bbefde8665c7620dbd83b718e4dce8
Maaz Ul Haq <maazulhaq00@gmail.com>
2026-04-22 12:41:28 +0500

    edit & del apis & templates

--- flask-crud-apis/app.py
+++ flask-crud-apis/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
+from bson import ObjectId 
 
 PORT=5001
 APP_URL=f"http://127.0.0.1:{PORT}"
@@ -33,6 +34,29 @@ def addstudent():
         return redirect(url_for("index"))
     
     return render_template("add-student.html")
+
+@app.route("/edit-student/<id>", methods=["GET", "POST"])
+def editstudent(id):
+
+    if request.method == "POST":
+        updated_student_data = {
+            "name": request.form["name"],
+            "email": request.form["email"],
+            "contact": request.form["contact"],
+            "course": request.form["course"],
+        }
+
+        students_collection.update_one({"_id": ObjectId(id)}, {"$set": updated_student_data})
+
+        return redirect(url_for("index"))
+
+    student = students_collection.find_one({"_id": ObjectId(id)})
+    return render_template("edit-student.html", student=student )
+
+@app.route("/delete-student/<id>")
+def deletestudent(id):
+    students_collection.delete_one({"_id": ObjectId(id)})
+    return redirect(url_for("index"))
 
 
 if __name__ == "__main__":
